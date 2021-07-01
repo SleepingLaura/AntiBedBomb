390abb361fa3dc535efe29bf23cc6ffa2f8ee35e
Laura <82168365+SleepingLaura@users.noreply.github.com>
2021-07-01 14:05:40 +0200

    Update AntiBedBomb.java

--- src/main/java/nl/sleepinglaura/antibedbomb/AntiBedBomb.java
+++ src/main/java/nl/sleepinglaura/antibedbomb/AntiBedBomb.java
@@ -4,12 +4,15 @@ import nl.sleepinglaura.antibedbomb.commands.MainCommand;
 import nl.sleepinglaura.antibedbomb.listeners.BedExplode;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+import nl.sleepinglaura.antibedbomb.bukkit.Metrics;
 
 public final class AntiBedBomb extends JavaPlugin implements Listener {
 
     public final BedExplode bedListener = new BedExplode(this);
 
     public void onEnable() {
+        int pluginId = 11898;
+        Metrics metrics = new Metrics(this, pluginId);
         saveDefaultConfig();
         this.getCommand("antibedbomb").setExecutor(new MainCommand(this));
         getServer().getPluginManager().registerEvents(this.bedListener, this);
